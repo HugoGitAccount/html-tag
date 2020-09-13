da9bed24ebd4d1a0390d42171b6390f8f44d638d
HugoGitAccount <dagostineh@gmail.com>
2020-09-13 22:27:13 +0100

    Added new files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function runCode() {
+    alert("Hello World! - HOLY BIBLE! (MATTHEW 25:1-13)");
+}
+
+const theEmail = 'abc123'; /*youtube: Visual Studio Code: HTML, CSS & JS Tips */
+
+type="text/javascript";
+var d=new Date();
+document.write('Today is ' + d + ' <br> ');
+
